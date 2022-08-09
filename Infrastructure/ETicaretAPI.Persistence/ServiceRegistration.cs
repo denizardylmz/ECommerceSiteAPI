6932ac143f48bcf1ae8587740b78b0fc3dfb30f5
denizyilmaz <deniz-arda-yilmaz@outlook.com>
2022-08-10 00:42:47 +0300

    Commit_2

--- Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -1,4 +1,10 @@
+using ETicaretAPI.Application.Repositories.CustomerRep;
+using ETicaretAPI.Application.Repositories.OrderRep;
+using ETicaretAPI.Application.Repositories.ProductRep;
 using ETicaretAPI.Persistence.Contexts;
+using ETicaretAPI.Persistence.Repository.CustomerRep;
+using ETicaretAPI.Persistence.Repository.OrderRep;
+using ETicaretAPI.Persistence.Repository.ProductRep;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +15,16 @@ public static class ServiceRegistration
     public static void AddPersistenceRegistration(this IServiceCollection collection)
     {
         collection.AddDbContext<ETicaretAPIDbContext>(options => 
-            options.UseNpgsql(Configurations.ConnectionString));
+            options.UseNpgsql(Configurations.ConnectionString), ServiceLifetime.Singleton);
+
+        collection.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
+        collection.AddSingleton<IProductReadRepository, ProductReadRepository>();
+        
+        collection.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
+        collection.AddSingleton<IOrderReadRepository, OrderReadRepository>();
+        
+        collection.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
+        collection.AddSingleton<ICustomerReadRepository, CustomerReadRepository>();
+        
     } 
 }
